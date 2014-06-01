f1d48ac9376a0c108a8355f44cf8c1b9d3168e19
groovyninja <roman.gordeev@mail.ru>
2014-06-01 14:41:28 +0400

    alternative solution

--- src/test/java/testBracketsTest.java
+++ src/test/java/testBracketsTest.java
@@ -23,6 +23,36 @@ public class testBracketsTest {
         assertFalse("not legal brackets", br.checkBrackets());
         br.setStr("(a*(b*(c*[d+n]-e*d{f}-g)");
         assertTrue("not legal brackets", br.checkBrackets());
+
+        br.setStr("(if (condition? x : y) sqrt(1 / x))");
+
+        assertTrue(br.checkBrackets());
+
+        br.setStr("Привет (тут я подумал (возможно) и передумал). (Возможно позже напишу)");
+
+        assertTrue(br.checkBrackets());
+
+        br.setStr(":-)");
+
+        assertFalse(br.checkBrackets());
+
+        br.setStr("())(");
+
+        assertFalse(br.checkBrackets());
+    }
+
+    @Test
+    public void checkBrackets()
+    {
+        Brackets b = new Brackets("");
+
+        assertTrue("сбалансировано: '(if (condition? x : y) sqrt(1 / x))'", b.checkBrackets("(if (condition? x : y) sqrt(1 / x))"));
+
+        assertTrue("сбалансировано: 'Привет (тут я подумал (возможно) и передумал). (Возможно позже напишу)'", b.checkBrackets("Привет (тут я подумал (возможно) и передумал). (Возможно позже напишу)"));
+
+        assertFalse("не сбалансировано: ':-)'", b.checkBrackets(":-)"));
+
+        assertFalse("не сбалансировано: '))('", b.checkBrackets("())("));
     }
 
 }
